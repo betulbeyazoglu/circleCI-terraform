db000a16d842641eeba88205b9ded52f8a8abae1
Betul <betul.nalan@gmail.com>
2021-03-18 16:02:50 -0400

    added aws context

--- main.tf
+++ main.tf
@@ -14,33 +14,6 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   }
-# resource "aws_iam_user" "circleci" {
-#   name = var.user
-#   path = "/system/"
-# }
-
-# resource "aws_iam_access_key" "circleci" {
-#   user = aws_iam_user.circleci.name
-# }
-
-# data "template_file" "circleci_policy" {
-#   template = file("circleci_s3_access.tpl.json")
-#   vars = {
-#     s3_bucket_arn = aws_s3_bucket.app.arn
-#   }
-# }
-
-# resource "local_file" "circle_credentials" {
-#   filename = "tmp/circleci_credentials"
-#   content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
-# }
-
-# resource "aws_iam_user_policy" "circleci" {
-#   name   = "AllowCircleCI"
-#   user   = aws_iam_user.circleci.name
-#   policy = data.template_file.circleci_policy.rendered
-# }
-
 resource "aws_instance" "project-bb" {
   ami = "ami-013f17f36f8b1fefb"
   instance_type = "t2.micro"
